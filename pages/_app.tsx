d7db42b2a25b70f8747f67357bc75751c045b1af
Jonathan Segal <jonathannsegal@gmail.com>
2024-02-02 10:50:21 -0500

    add speedlights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Heebo } from 'next/font/google';
 
 const heebo = Heebo({ subsets: ['latin'] })
@@ -11,6 +12,7 @@ export default function App({Component, pageProps}: AppProps) {
     <ThemeProvider attribute='class'>
       <main className={heebo.className}>
         <Component {...pageProps} />
+        <SpeedInsights />
       </main>
     </ThemeProvider>
   );
